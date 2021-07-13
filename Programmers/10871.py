a604587aae09cabf198eed4c8174acfd1285b9f9
jieul2 <jieul2@kakao.com>
2021-07-13 23:04:06 +0900

    .

--- Programmers/10871.py
+++ Programmers/10871.py
@@ -1,4 +1,11 @@
 a,b = map(int,input('').split())
-n = []
-for i in range(a):
-    n.append(input())
+n = list(map(int,input().split()))
+# a = 10
+# b = 5
+# n = [1, 10, 4, 9, 2, 3, 8, 5, 7, 6]
+answer = ''
+for i in n:
+    if i < b:
+        answer += str(i) + ' '
+
+print(answer[:-1])
