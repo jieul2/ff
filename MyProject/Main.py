d0163d7d686b51c77536531f95863893b2f641cb
jieul2 <null>
2021-07-25 04:23:00 +0000

    Project

--- MyProject/Main.py
+++ MyProject/Main.py
@@ -1,5 +1,10 @@
-class Main():
-    print("Hello World")
-    
-    
-Main()
+class Player():
+    def __init__(self, name, level, money):
+        self.name = name
+        self.level = level
+        self.money = money
+        print(f'{name}님 {level}레벨 {money}원')
+
+        
+p1 = Player('da', '0', '10000')
+p2 = Player('na', '1', '10000')
